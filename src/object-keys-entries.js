a4092ea84b170525fa357d0ec92a2e323dc65ed8
David Delgadillo <daviddel.731@gmail.com>
2021-05-07 16:24:56 -0700

    code challenges 03 completed

--- src/object-keys-entries.js
+++ src/object-keys-entries.js
@@ -1,4 +1,30 @@
 export function capitalizeObjectKeys(obj) {
   const result = Object.keys(obj);
-  return result.toUpperCase();
+  return result.map(x => x.toUpperCase());
+}
+
+export function sortedKeys(obj) {
+  const keys = Object.keys(obj);
+  keys.sort(function(a, b) {
+    return a.length - b.length;
+  });
+  return keys;
+}
+
+export function getFilteredKey(obj) {
+  const keys = Object.keys(obj);
+  const array = [];
+  array.push(keys[2]);
+  return array;
+}
+
+export function getArrayOfKeysAndValues(obj){
+  const object = Object.entries(obj);
+  return object;
+}
+
+export function sortedArraysByValuesLength(obj) {
+  const object = Object.entries(obj);
+  console.log(Object.values(obj));
+  return object.sort((a, b) => (a.length > b.length) ? 1 : -1);
 }
--- src/object-keys-entries.test.js
+++ src/object-keys-entries.test.js
@@ -1,7 +1,31 @@
-import { capitalizeObjectKeys } from './object-keys-entries.js';
+import { capitalizeObjectKeys, getArrayOfKeysAndValues, getFilteredKey, sortedArraysByValuesLength, sortedKeys } from './object-keys-entries.js';
 
 test('it capitalizes the keys', () => {
   const dataObject = { name: 'Angelina Jolie', age: 80 }; //arrange
   const output = capitalizeObjectKeys(dataObject); //act
   expect(output).toEqual(['NAME', 'AGE']); //assert
+});
+
+test('keys sorted smallest to largest', () => {
+  const dataObject = { name: 'Spot', age: 3, isDog: true, friends: ['Rover', 'Steve', 'Fluffy'] }; //arrange
+  const output = sortedKeys(dataObject); //act
+  expect(output).toEqual(['age', 'name', 'isDog', 'friends']); //assert
+});
+
+test('returns the key age', () => {
+  const dataObject = { name: 'Angelina Jolie', isSpot: false, age: 80, }; //arrange
+  const output = getFilteredKey(dataObject); //act
+  expect(output).toEqual(['age']); //assert
+});
+
+test('returns n ew array of key/value pairs', () => {
+  const dataObject = { name: 'Angelina Jolie', age: 80 }; //arrange
+  const output = getArrayOfKeysAndValues(dataObject); //act
+  expect(output).toEqual([['name', 'Angelina Jolie'], ['age', 80]]); //assert
+});
+
+test('return an array of arrays sorted longest to shortest', () => {
+  const dataObject = { name: 'Bob', friend: 'Tom Hanks', location: 'Los Angeles' }; //arrange
+  const output = sortedArraysByValuesLength(dataObject); //act
+  expect(output).toEqual([['location', 'Los Angeles'], ['friend', 'Tom Hanks'], ['name', 'Bob']]); //assert
 });
